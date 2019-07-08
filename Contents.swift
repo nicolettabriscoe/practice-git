2e451fb08126a560904708a3f521aa1b0785f5a3
Apple <apple@Apples-MacBook-Pro-4.local>
2019-07-08 15:36:31 -0500

    Second Commit

---
+++ Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+//var str = "Hello, playground"
+//
+//var name1 = "Nailah"
+//var name2 = "Sabyatha"
+//
+//print("Hi \(name1) and \(name2)! We are so happy you're here at KWK Portland!")
+
+//intention: working with integers and doubles
+//data types: ints and doubles
+
+//var int = 4
+//var double = 5.0
+//
+//print(Double(int) * double)
+
+//var a = 12.0
+//var b = 65
+//var c = 31.0
+//var d = 98.0
+//
+//print(((a+b+c)+Double(d))/4)
+
